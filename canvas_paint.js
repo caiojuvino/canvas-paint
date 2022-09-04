17d333d7470196971926f12a8f5ada88cff32690
Caio Fábio Juvino de Queiroz <caio.queiroz@ccc.ufcg.edu.br>
2022-09-03 21:11:14 -0300

    Get mouse position on canvas

--- canvas_paint.js
+++ canvas_paint.js
@@ -14,3 +14,16 @@ function clear() {
 }
 
 window.onresize = () => setSize();
+
+canvas.addEventListener("click", function (evt) {
+    var mousePos = getMousePos(canvas, evt);
+    alert(mousePos.x + ',' + mousePos.y);
+}, false);
+
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+    };
+}
